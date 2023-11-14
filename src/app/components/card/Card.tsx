49c5f9776251294be190f4493d3c6cfc0333752d
gabriel-a-fernandes <gabriel-a-fernandes@hotmail.com>
2023-11-13 21:25:38 -0300

    Estilizando e atualizando card

--- src/app/components/card/Card.tsx
+++ src/app/components/card/Card.tsx
@@ -1,9 +1,11 @@
+import styled from "./Card.module.css"
+
 export default function Card() {
     return (
       <>
-        <h1> Título </h1>
-        <p> Imagem </p>
-        <p> Conteúdo </p>
+        <h1 className={styled.cardTitle}> Título </h1>
+        <p className={styled.cardImage}> Imagem </p>
+        <p className={styled.cardContent}> Conteúdo </p>
       </>
     );
 }
